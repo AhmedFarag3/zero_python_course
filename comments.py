268d516b59c9f7e517822d1d695f28e49997c1d5
ahmed farag <faragahmed99@gmail.com>
2021-04-23 14:09:37 +0200

    comments

---
+++ comments.py
@@ -0,0 +1,9 @@
+#---------------------
+#info about the file
+#License
+#who created the file 
+#When the file is created
+#why the file is created
+#prevent code form run 
+#------------------------
+print("I love python")
