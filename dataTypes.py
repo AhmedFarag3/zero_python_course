4b12af58a1e0dfe42fd81907ce6e92799212d43f
ahmed farag <faragahmed99@gmail.com>
2021-04-23 14:36:47 +0200

    DataTypes

---
+++ dataTypes.py
@@ -0,0 +1,12 @@
+#----------------------
+#type()
+#All data in python is object
+#---------------------------
+
+print(type(10)) # int => integer
+print(type(10.9)) # float -> float
+print(type("hello python")) #string 
+print(type([10,5])) #list
+print(type((1,2,3))) # tuple
+print(type({"one":1 , "two":2,"three":3}))#dict
+print(type(2==2))#boolean
